687663352f99d4058a78add3b0b08e96af4f28f2
bryankier <bkleo.tech@gmail.com>
2025-03-11 23:10:56 +0800

    Chapter 3: DONE

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 
 from stats import word_counter, char_counter, sort_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 
 def get_book_text(path):
     with open(path) as file:
         return file.read()
 
-path = "books/frankenstein.txt"
+path = sys.argv[1]
 text = get_book_text(path)
 num_words = word_counter(text)
 char_dict = char_counter(text)
